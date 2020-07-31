eb1d8b5e90cf779a541e97302a9bdb0ef775bc01
Stéphane Tatin <stephane.tatin@gmail.com>
2020-07-31 13:38:53 +0200

    deploy 1.3

--- client/src/store.js
+++ client/src/store.js
@@ -6,9 +6,11 @@ const initialState = {};
 
 const middleWare = [thunk];
 
-var store = createStore(rootReducer, initialState, compose(
-    applyMiddleware(...middleWare),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+  rootReducer,
+  initialState,
+  composeEnhancers(applyMiddleware(...middleWare))
+);
 
 export default store;
